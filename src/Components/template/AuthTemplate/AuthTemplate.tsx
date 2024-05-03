097be03f6e3c49ad01b4c5796cc3467c1c5ba0bb
kuldip-trentium <kuldip@trentiums.com>
2024-05-03 10:26:54 +0530

    fix css

--- src/Components/template/AuthTemplate/AuthTemplate.tsx
+++ src/Components/template/AuthTemplate/AuthTemplate.tsx
@@ -21,7 +21,7 @@ const AuthTemplate: React.FC<AuthTemplateProps> = ({
       <Row className="auth-row">
         <Col
           md={6}
-          className="left-column col-md-12 col-lg-6 col-xl-6 col-xxl-6 d-flex justify-content-start align-items-center flex-column">
+          className="left-column col-md-12 col-lg-6 col-xl-6 col-xxl-6 d-flex justify-content-start align-items-center flex-column p-0 m-0">
           <div className="logo-view">
             <img src={logo} height={20} width={100} alt="logo" />
           </div>
